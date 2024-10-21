196a32c1254452056068d59583ea957dbc5a0ad8
Mikaela <G00401392@atu.ie>
2024-10-21 16:38:38 +0100

    VehicleRepository is now an interface and I added code to extend JpaRepository

--- src/main/java/ie/atu/labexam1vehiclemanagementapplication/VehicleRepository.java
+++ src/main/java/ie/atu/labexam1vehiclemanagementapplication/VehicleRepository.java
@@ -1,4 +1,9 @@
 package ie.atu.labexam1vehiclemanagementapplication;
 
-public class VehicleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository <Vehicle, Long> {
+
 }
