cceb2636b97655652db0dadc7f7034348d7b9d4e
Mikaela <G00401392@atu.ie>
2024-10-21 16:19:56 +0100

    Created new project

---
+++ src/main/java/ie/atu/labexam1vehiclemanagementapplication/LabExam1VehicleManagementApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam1vehiclemanagementapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExam1VehicleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabExam1VehicleManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/labexam1vehiclemanagementapplication/LabExam1VehicleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam1vehiclemanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExam1VehicleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
